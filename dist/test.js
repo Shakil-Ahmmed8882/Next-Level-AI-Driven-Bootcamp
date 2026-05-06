c6b314e2325ae79a17e4280f42a0a2351c1bcf1b
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2026-05-06 07:29:56 +0600

    Add TypeScript source files and configuration for basic types exploration

---
+++ dist/test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const course = "TypeScript Basics  - Learn the fundamentals of TypeScript";
+const age = 33;
+console.log({ course, age });
+//# sourceMappingURL=test.js.map
---
+++ src/explore-basic-types-of-typescript/test.ts
@@ -0,0 +1,3 @@
+const course : string = "TypeScript Basics  - Learn the fundamentals of TypeScript";
+const age : number = 33;
+console.log({course, age});
